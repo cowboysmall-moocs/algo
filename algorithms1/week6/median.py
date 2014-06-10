74c1ed9ebf29ef63a2c2e64d69a4c2e75bb8ac1b
Jerry Kiely <jerry@cowboysmall.com>
2014-06-10 00:28:34 -0400

    added week 6 part ducks - completed two sum + median maintenance

---
+++ algorithms1/week6/median.py
@@ -0,0 +1,42 @@
+import sys
+import heapq
+
+
+def construct_stream(file_path):
+    with open(file_path) as file:
+        return [int(line) for line in file]
+        # lines = file.readLines()
+    # return [int(line) for line in lines]
+
+
+def median_maintenance(stream):
+    total = 0
+    upper, lower = [], []
+    for integer in stream:
+        if not lower or integer < -lower[0]:
+            heapq.heappush(lower, -integer)
+        else:
+            heapq.heappush(upper, integer)
+
+        diff = len(upper) - len(lower)
+        if diff > 1:
+            heapq.heappush(lower, -heapq.heappop(upper))
+        elif diff < -1:
+            heapq.heappush(upper, -heapq.heappop(lower))
+
+        if len(upper) > len(lower):
+            total += upper[0]
+        else:
+            total += -lower[0]
+    return total
+
+
+def main(argv):
+    sum_of_medians = median_maintenance(construct_stream(argv[0]))
+
+    print 'Sum of medians             = %8d' % (sum_of_medians)
+    print 'Sum of medians (Mod 10000) = %8d' % (sum_of_medians % 10000)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ algorithms1/week6/two_sum.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def construct_hash_table(file_path):
+    hash_table = {}
+    with open(file_path) as file:
+        for line in file:
+            hash_table[int(line)] = 0
+    return hash_table
+
+
+def two_sum(hash_table):
+    counter = 0
+    hash_keys  = hash_table.keys()
+    for val in range(-10000, 10001):
+        for number in hash_keys:
+            target = val - number
+            if target != number and target in hash_table:
+                counter += 1
+                sys.stdout.write('Found T = %6d [running total = %6d]\r' % (val, counter))
+                sys.stdout.flush()
+                break
+    return counter
+
+
+def main(argv):
+    count_of_two_sums = two_sum(construct_hash_table(argv[0]))
+
+    print '\n\nNumber of Ts with distinct solutions: %8d' % (count_of_two_sums)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
